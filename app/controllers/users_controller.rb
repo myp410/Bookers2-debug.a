9fee3536095ddf123263586ad2ffe4380c478e55
EC2 Default User <ec2-user@ip-172-31-46-100.ap-northeast-1.compute.internal>
2024-03-24 12:10:54 +0000

    応用課題８a DM機能実装中エラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,11 @@ class UsersController < ApplicationController
     
     @currentUserEntry = Entry.where(user_id: current_user.id)
     @userEntry = Entry.where(user_id: @user.id)
-    unless @user.id == current_user.id
+      if @user.id == current_user.id
+      else  
       @currentUserEntry.each do |cu|
         @userEntry.each do |u|
-          if cu.room_id == u.room_id
+          if cu.room_id == u.room_id then
             @isRoom = true
             @roomId = cu.room_id
           end
